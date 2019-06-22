b9b5633c96f645a8f9bfc0fc65d06a8983db57f0
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-22 19:23:55 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+var el = document.getElementById('main');
+el.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'Nick is the champion'
+document.body.appendChild(newHeader)
